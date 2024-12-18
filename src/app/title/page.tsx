1a3fd7a25dd1b63661ce9f8e1c1840b7fea58208
S-Ou <samuel.ou03@gmail.com>
2024-12-19 01:30:16 +1300

    feat: sequential order css

--- src/app/title/page.tsx
+++ src/app/title/page.tsx
@@ -80,6 +80,46 @@ const DependencyTabStyle = styled.div`
 	}
 `;
 
+const SequentialOrderBlock = styled.div`
+	align-items: center;
+	display: flex;
+	flex-direction: row;
+	gap: 8rem;
+	height: 50rem;
+	max-width: 150rem;
+`;
+
+const SequentialItem = styled.div`
+	align-items: center;
+	display: flex;
+	height: 100%;
+	justify-content: center;
+	flex-direction: column;
+	gap: 1rem;
+
+	img {
+		height: 100%;
+		object-fit: contain;
+	}
+
+	h2 {
+		font-size: 2rem;
+		font-weight: bold;
+		opacity: 0.8;
+	}
+
+	h3 {
+		font-size: 1.5rem;
+		font-weight: normal;
+		opacity: 0.6;
+	}
+`;
+
+const SequentialAdjacent = styled(SequentialItem)<{ alignRight?: boolean }>`
+	height: 50%;
+	${({ alignRight }) => (alignRight ? "margin-left" : "margin-right")}: auto;
+`;
+
 const ButtonsList = styled.div`
 	align-items: flex-end;
 	display: flex;
@@ -185,13 +225,38 @@ const DependencyOrder: React.FC<DependencyOrderProps> = ({ title }) => {
 	);
 };
 
-interface SequentialTabProps {
-	previous?: PreviewTitleDto;
-	next?: PreviewTitleDto;
+interface SequentialOrderProps {
+	title: SequentialOrderTitleDto;
 }
 
-const SequentialOrder: React.FC<SequentialTabProps> = ({ previous, next }) => {
-	return <></>;
+const SequentialOrder: React.FC<SequentialOrderProps> = ({ title }) => {
+	const { previous, next } = title;
+
+	return (
+		<SequentialOrderBlock>
+			<SequentialAdjacent alignRight={true}>
+				{previous && (
+					<>
+						<h2>previous</h2>
+						<Poster src={previous.smallPosterUrl} alt={previous.name} />
+						<h3>{previous.releasedAtUtc?.getFullYear()}</h3>
+					</>
+				)}
+			</SequentialAdjacent>
+			<SequentialItem>
+				<LeadPoster src={title.largePosterUrl} alt={title.name} />
+			</SequentialItem>
+			<SequentialAdjacent alignRight={false}>
+				{next && (
+					<>
+						<h2>next</h2>
+						<Poster src={next.smallPosterUrl} alt={next.name} />
+						<h3>{next.releasedAtUtc?.getFullYear()}</h3>
+					</>
+				)}
+			</SequentialAdjacent>
+		</SequentialOrderBlock>
+	);
 };
 
 export default function Title() {
@@ -290,7 +355,7 @@ export default function Title() {
 			name: "Iron Man 3",
 			type: "movie",
 			smallPosterUrl:
-				"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/1Jj7Frjjbewb6Q6dl6YXhL3kuvB.jpg",
+				"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/qhPtAc1TKbMPqNvcdXSOn9Bn7hZ.jpg",
 			releasedAtUtc: new Date("2013-04-18"),
 		},
 	};
@@ -304,10 +369,7 @@ export default function Title() {
 				{/* temporary ^^ */}
 			</TitleNameBlock>
 			{orderType === "sequential" ?
-				<SequentialOrder
-					previous={dummyTitleSequential.previous}
-					next={dummyTitleSequential.next}
-				/>
+				<SequentialOrder title={dummyTitleSequential} />
 			:	<DependencyOrder title={dummyTitleDependency} />}
 
 			<DetailsBlock>
