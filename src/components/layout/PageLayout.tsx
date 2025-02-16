334e661c59577fdb7801ebbfad80a52d539d5fbc
pumbas600 <joshjeffers600@gmail.com>
2025-02-16 13:43:58 +1300

    style: make title page use page layout

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -4,8 +4,10 @@ const PageLayout = styled.div`
 	display: flex;
 	flex-direction: column;
 	gap: 2rem;
+	margin-block: 2rem;
 	margin-inline: auto;
 	max-width: 62.5rem;
+	padding-inline: 1rem;
 `;
 
 export default PageLayout;
