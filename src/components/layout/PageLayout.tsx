5fa7777ffa4b41e90df5d8e83939624ad481c4da
pumbas600 <joshjeffers600@gmail.com>
2025-02-16 13:38:45 +1300

    style: create page layout component and fix summary page

---
+++ src/components/layout/PageLayout.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const PageLayout = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 2rem;
+	margin-inline: auto;
+	max-width: 62.5rem;
+`;
+
+export default PageLayout;
